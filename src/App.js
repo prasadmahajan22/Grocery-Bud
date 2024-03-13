81d7e3144d9021b4c3b9034689f81c790758f9a8
prasadmahajan22 <prasadmahajan440@gmail.com>
2024-03-13 22:43:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Form } from "./Components/Form";
+
+function App() {
+
+  return (
+    <div className="wrapper">
+      <h3>Grocery Bud</h3>
+      <div className="form-and-todo-box">
+        <Form/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
